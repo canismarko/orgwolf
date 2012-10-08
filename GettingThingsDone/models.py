4a4e81f0c816844bb28b5167467d2e3beeb58ce1
Mark Wolf <mark.wolf.music@gmail.com>
2012-10-07 23:31:26 -0400

    GTD models, first syncdb and added a few sets of ideas to project-overview.org
    Getting Things Done models were added that contain basic functionality to use org-mode. GTD models validate but have not been thoroughly tested. Some of the brainstorming ideas were added to the project overview. Settings file was changed to add GettingThingsDone module.

--- GettingThingsDone/models.py
+++ GettingThingsDone/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# Create your models here.
+from django.contrib.auth.models import User
 
 class TodoState(models.Model):
     abbreviation = models.CharField(max_length=10)
@@ -9,41 +9,82 @@ class TodoState(models.Model):
 
 class Tag(models.Model):
     display = models.CharField(max_length=100)
-    value = models.CharField(max_length=10)
+    tag_string = models.CharField(max_length=10)
+    owner = models.ForeignKey(User, blank=True, null=True) # no owner means built-in tag
+    public = models.BooleanField(default=True)
 
 class Tool(Tag):
+    pass
     
 class Location(Tag):
+    GPS_info = False # TODO
+    tools_available = models.ManyToManyField('Tool', related_name='including_locations_set')
+    tools_unavailable = models.ManyToManyField('Tool', related_name='excluding_locations_set')
+    people_available = models.ManyToManyField('Contact', related_name='including_locations_set')
 
 class Contact(Tag):
+    f_name = models.CharField(max_length = 50)
+    l_name = models.CharField(max_length = 50)
+    user = models.ForeignKey('auth.user', blank=True, null=True)
+    # message_contact = models.ForeignKey('messaging.contact', blank=True, null=True) # TODO: uncomment this once messaging is implemented
 
 class Context(models.Model):
-    tools_available = models.ManyToManyField('Tool')
-    tools_unavailable = models.ManyToManyField('Tool')
-    locations_available = models.ManyToManyField('Location')
-    locations_unavailable = models.ManytoManyField('Location')
+    tools_available = models.ManyToManyField('Tool', related_name='including_contexts_set')
+    tools_unavailable = models.ManyToManyField('Tool', related_name='excluding_contexts_set')
+    locations_available = models.ManyToManyField('Location', related_name='including_contexts_set')
+    locations_unavailable = models.ManyToManyField('Location', related_name='excluding_contexts_set')
+    people_available = models.ManyToManyField('Contact', related_name='including_contexts_set')
+    people_unavailable = models.ManyToManyField('Contact', related_name='excluding_contexts_set')
+    def get_actions_list(self):
+        """
+        Retrieve all the actionable items given the current context. 
+        Polls all objects of the Node class and selects based on the
+        tag_string of the Node.
+        """
+        pass # TODO
 
-class Heading(models.Model):
+class Priority(models.Model):
+    priority_value = models.IntegerField(default=50)
+    owner = models.ForeignKey(User)
+
+class Scope(models.Model):
+    owner = models.ForeignKey(User, blank=True, null=True) # no owner means built-in tag
+    public = models.BooleanField(default=True)
+    display = models.CharField(max_length=50)
+
+class Node(models.Model):
     """
     Django model that holds some sort of divisional heading. Similar to orgmode '*** Heading'
-    syntax. It can have todo states associated with it as well as scheduling and other information.
+    syntax. It can have todo states associated with it as well as scheduling and other information. Each Node object must be associated with a project. A project is a Node with no parent (a top level Node)
     """
-    # Determine where this heading is
-    parent = models.ForeignKey('self')
+    text = models.TextField()
     todo_state = models.ForeignKey('TodoState', blank=True, null=True)
+    # Determine where this heading is
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='child_heading_set')
+    project = models.ManyToManyField('Project', related_name='project_heading_set') # should this be ForeignKey?
     # Scheduling details
     scheduled_date = models.DateField(blank=True, null=True)
     closed_date = models.DateField(blank=True, null=True)
     deadline_date = models.DateField(blank=True, null=True)
-    scheduled_datetime = models.DateTimefield(blank=True, null=True)
-    closed_datetime = models.DateTimefield(blank=True, null=True)
-    deadline_datetime = models.DateTimefield(blank=True, null=True)
-    # Miscellaneous details
+    scheduled_datetime = models.DateTimeField(blank=True, null=True)
+    closed_datetime = models.DateTimeField(blank=True, null=True)
+    deadline_datetime = models.DateTimeField(blank=True, null=True)
+    repeating_number = models.IntegerField(blank=True, null=True)
+    repeating_unit = models.CharField(max_length=1, blank=True, null=True)
+    repeating_strict_mode = models.NullBooleanField(default=True)
+    # Selection criteria
     priority = models.ForeignKey('Priority', blank=True, null=True)
-    context = models.ForeignKey('Context')
-    contact = models.ManyToManyField('Contact')
-    tag_string = models.TextField() # Org-mode style string (eg ":comp:-phone:")
-    # These methods provide statuses of this object
+    tag_string = models.TextField() # Org-mode style string (eg ":comp:home:RN:")
+    energy = models.CharField(max_length=2, blank=True, null=True,
+                              choices=(('High', 'HI'),
+                                       ('Low', 'LO'))
+                              )
+    time_needed = models.CharField(max_length=4, blank=True, null=True,
+                                   choices=(('High', 'HI'),
+                                            ('Low', 'LO'))
+                                   )
+    scope = models.ManyToManyField('Scope')
+    # Methods retrieve statuses of this object
     def is_todo(self):
         if self.todo_state:
             return True
@@ -59,10 +100,51 @@ class Heading(models.Model):
             return todo_state.done
         else:
             return False
-    # Get the tags associated with this heading
-    def get_tags(self):
+    # Methods manipulate the context information
+    def _get_tags(self):
         tags_list = self.tag_string.split(":")
-        return tags_list[1:len(tags_list-1) # Get rid of the empty first and last elements
+        return tags_list[1:len(tags_list-1)] # Get rid of the empty first and last elements
+    def add_context_item(self, new_item):
+        """Add a required Person, Tool or Location to this Node"""
+        pass # TODO
+    def rm_context_item(self, item_to_remove):
+        """Remove a required Person, Tool or Location from this Node"""
+        pass # TODO
+    def get_context_items(self):
+        """
+        Return a list of Person, Tool and/or Location objects required
+        for this Node. Usually associated with a TODO item. Empty list
+        if none.
+        """
+        return []
+    # Methods return miscellaneous information
+    def get_text(self):
+        """Get any text directly associated with this node. False if none."""
+        # TODO
+        return False
+    def get_children(self):
+        """Returns a list of Node objects with this Node as its parent."""
+        return []
+
+class Project(models.Model):
+    """
+    A project is defined as a Node that has no parent. An isolated TODO
+    item is de-facto classified as a project. This may seem confusing but
+    practically it does not pose any problems. This is a sub-classed version
+    of the Node without the requirement that it have a parent and
+    with a few extra method attributes specific only to projects.
+    """
+    title = models.TextField()
+    owner = models.ManyToManyField('auth.User', related_name='owned_project_set')
+    other_users = models.ManyToManyField('auth.User', related_name='other_project_set')
+    def get_num_actions(self):
+        pass
+    # TODO: brainstorm more methods
+
 class Text(models.Model):
-    parent = models.ForeignKey('Heading')
+    """
+    Holds the text component associated with a Node object.
+    """
+    # TODO: Add support for lists, tables, etc.
+    parent = models.ForeignKey('Node', related_name='attached_text')
 
