13076d58743dd2fe171eb7e5b13981589a5eceef
Mark Wolf <mark.wolf.music@gmail.com>
2012-12-08 09:59:19 +0900

    Incomplete modifications to hierarchy node view.
    Submitted to stack overflow.
    
    Tried to make the javascript more modular for the expanding nodes
    hierarchy view. The `.click()` handler does not seem to stay connected.
    The problem was submitted to stack overflow:
    http://stackoverflow.com/questions/13773260/

--- gtd/static/node_view.js
+++ gtd/static/node_view.js
@@ -5,6 +5,7 @@ var workspace_string = '';
 function heading(args) {
     // CSS classes
     this.ICON = 'icon-chevron-right'; // Eg. Bootstrap glyphicon
+    this.ICON_WIDTH; // Set dynamically at onReady()
     this.COLORS = new Array('red', 'green', 'blue');
     if(typeof args['heading_div'] != 'undefined') {
 	// Allow a selected JQuery element to be passed
@@ -22,6 +23,7 @@ function heading(args) {
     this.id = args['node_id'];
     this.title = args['html'];
     this.tags = args['tags'];
+    this.text = args['text'];
     if(typeof args['parent_id'] == 'undefined') {
 	args['parent_id'] = '0';
     }
@@ -36,7 +38,7 @@ function heading(args) {
 	this.level = Number(this.parent.attr('level')) + 1;
     }
     if (this.exists) {
-	this.element = $('#heading'+this.id);
+    	this.selector = '#heading'+this.id;
     }
     // Method converts a heading to it's HTML form (including divs)
     this.as_html = function() {
@@ -52,15 +54,19 @@ function heading(args) {
 	var color_i = this.level % this.COLORS.length;
 	string += ' style="color: ' + this.COLORS[color_i] + ';"';
 	string += '>\n';
-	string += this.spacers();
-	string += '<i class="' + this.ICON + '"></i>\n';
+	string += '<i class="' + this.ICON + ' twisty"></i>\n';
 	string += this.title;
 	string += this.tags;
 	string += '</div>\n';
 	string += '<div class="children';
 	string += '">\n';
+	if( typeof this.text != 'undefined' && this.text != '' ) {
+	    string += '<div class="heading_text">\n';
+	    string += this.text + '\n';
+	    string += '</div>\n';
+	}
 	string += '<div class="loading">';
-	string += this.spacers(1) + '<em>Loading...</em></div>\n';
+	string += '<em>Loading...</em></div>\n'; // Todo: Add indentation to the loading element
 	string += '</div>\n'
 	string += '</div>';
 	return string
@@ -73,61 +79,70 @@ function heading(args) {
 	html += this.as_html()
 	container.html(html);
 	// Now set data
-	this.element = $('#heading' + this.id);
+	this.$element = $('#heading' + this.id);
 	this.exists = true;
-	this.element.attr('level', this.level);
-	this.element.attr('node_id', this.id);
-	this.element.attr('tags', this.tags);
-	this.element.attr('parent_id', this.parent.id);
-	this.element.attr('title', this.title);
+	this.$element.attr('level', this.level);
+	this.$element.attr('node_id', this.id);
+	this.$element.attr('tags', this.tags);
+	this.$element.attr('parent_id', this.parent.id);
+	this.$element.attr('title', this.title);
+	this.$element.children('.clickable').click(function() {
+	    alert('clicked: ' + $(this).attr('id') + '.' + $(this).attr('class'));
+	});
+	this.$element.children('.clickable').click(); // This works
     }
-    this.spacers = function(offset) {
+    this.set_indent = function($element, offset) {
+	// Method accepts a JQuery selector and an optional offset and
+	// sets margin-left based on this.level
 	if (typeof offset == 'undefined') {
 	    offset = 0;
-	}
-	var string = '';
-	for(i=0; i<(this.level+offset); i++) {
-	    string += '<i class="' + this.ICON + ' spacer"></i>\n';
-	}
-	return string;
-    };	
+	    }
+	var icon_width = $('body').data('icon_width') + 4;
+	var new_margin = offset * icon_width;
+	$element.css('margin-left', new_margin + 'px');
+    };
     this.populate_children = function() {
 	// Call ajax method to get the children of the current heading
 	$.ajaxSetup({
 	    headers: { "X-CSRFToken": $.cookie('csrftoken')}
 	});
 	// First get the necessary data from server
-	$.post('/ajax/gtd/get_heading.json', 
+	$.post('/ajax/gtd/get_heading.json',
 	       { parent_id: this.id },
 	       function(response) {
 		   parent_selector = '#heading' + response.data.parent_id;
-		   var parent = $(parent_selector);
-		   var parent_heading = new heading({heading_div: parent});
-		   var children_div = $(parent_selector).children('div.children');
+		   var $parent = $(parent_selector);
+		   var parent_heading = new heading({heading_div: $parent});
+		   var $children_div = $(parent_selector).children('div.children');
 		   // Then process each child heading
 		   for(i in response.data.children) {
-		       child_data = response.data.children[i];
+		       var child_data = response.data.children[i];
 		       child_data['parent_id'] = response.data.parent_id;
 		       child = new heading(child_data);
-		       child.create_new({container: children_div});
+		       child.create_new({container: $children_div});
 		   }
 		   // Add a line to add new headings
 		   var add_html = '';
 		   add_html += '<div class="add_heading">\n';
-		   add_html += parent_heading.spacers(1);
 		   add_html += '<i class="icon-plus-sign"></i> [New heading]\n';
-		   children_div.html(children_div.html() + add_html);
+		   $children_div.html($children_div.html() + add_html);
+		   parent_heading.set_indent($children_div, 1);
+		   var $add_heading = $parent.children('.children').children('.add_heading');
 		   // Remove the "Loading..." message
-		   children_div.children('.loading').remove();
+		   $children_div.children('.loading').remove();
+		   // Connect the add_heading to its click function
+		   $add_div = $parent.children('.children').children('.add_heading');
+		   $add_div.click(function() {
+		       alert('New child of ' + parent_heading.id);
+		       });
 	       }
 	      );
 	// Mark this element as populated
-	this.element.data('populated', true);
     };
     // Method cycles between open and closed states
     this.toggle = function() {
 	element = $('#heading' + this.id);
-	icon = element.children('.clickable').children('i');
+	icon = element.children('.clickable').children('i.twisty');
 	if (icon.hasClass('icon-chevron-right')) {
 	    icon.removeClass('icon-chevron-right');
 	    icon.addClass('icon-chevron-down');
@@ -142,43 +157,43 @@ function heading(args) {
 	    if($(this).data('populated') != true) {
 		this_heading = new heading({heading_div: $(this)});
 		this_heading.populate_children();
-		$(this).children('.clickable').click(function() {
-		    new_heading = new heading({heading_div: $(this).parent()});
-		    new_heading.toggle();
-		});
 	    }
 	});
     };
 };
 
 $(document).ready(function() {
-    // Convert the children nodes to an outline view
-    var workspace = $('#node-workspace');
-    workspace.attr('node_id', '0');
-    workspace.attr('level', '-1');
-    workspace.attr('id', 'heading0');
-    workspace.css('visibility', 'hidden');
-    $('tr.child_row').each(function() {
-	var row_id = $(this).attr('node_id');
-	var row = new heading({
-	    node_id: row_id,
-	    html: $(this).children('#title'+row_id).children('a').html(),
-	    tags: $(this).children('#tags'+row_id).html(),
-	    parent_id: '0'
-	});
-	row.create_new({container: workspace});
+    // Determine the width of the chevron icon
+    $('body').html($('body').html() + '<i id="test-icon""></i>');
+    var $test_icon = $('#test-icon');
+    $test_icon.css('display', 'none');
+    $test_icon.addClass('icon-chevron-right');
+    $('body').data('icon_width', Number($test_icon.css('width').slice(0,-2)));
+    // Find and populate the workspace
+    var $old_table = $('#non-js-table');
+    var $workspace = $('#node-workspace');
+    $workspace.html('<div class="children"></div>\n');
+    if ($old_table.attr('node_id')) {
+	$workspace.attr('node_id', $old_table.attr('node_id'));
+    }
+    else {
+	$workspace.attr('node_id', '0');
+    }
+    $workspace.attr('level', '-1');
+    $workspace.attr('id', 'heading'+$workspace.attr('node_id'));
+    $workspace.css('visibility', 'hidden');
+    var workspace = new heading({
+	heading_div: $workspace,
     });
-    workspace.css('visibility', 'visible');
+    workspace.populate_children();
+    var $children = $workspace.children('.children');
+    $children.css('display', 'block');
+    $workspace.css('visibility', 'visible');
     $('#non-js-table').remove();
-    // Connect the click the event for a heading to its toggle function
-    $('.clickable').click(function() {
-	new_heading = new heading({heading_div: $(this).parent()});
-	new_heading.toggle();
-    });
 });
 
-// Once the doument loads then go about grabbing the first level of children
 window.onload = function() {
+    // Once the doument loads then go about grabbing the first level of children
     $('div.heading').each(function() {
 	var row = new heading({heading_div: $(this)});
 	row.populate_children();
