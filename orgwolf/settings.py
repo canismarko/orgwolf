23cb539995d299e9918ff23eec97cb69f9025046
Mark Wolf <mark.wolf.music@gmail.com>
2013-01-19 23:32:22 -0500

    Added support for south database migration (gtd app only)

--- orgwolf/settings.py
+++ orgwolf/settings.py
@@ -158,6 +158,7 @@ INSTALLED_APPS = (
     'gtd',
     'plugins',
     'wolfmail',
+    'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
